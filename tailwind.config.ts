6d4bbc27821f186ee83c65405a79256d6ad4a024
christoph-codes <tkcwebdev@gmail.com>
2024-04-20 19:41:49 -0700

    main: Add primary images used for landing pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,22 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#3F414C",
+				},
+				secondary: {
+					DEFAULT: "#9F9FAA",
+					light: "#F8F8F8",
+				},
+			},
 			backgroundImage: {
 				landing_hero: "url('/landing_hero.png')",
+				what_we_do_hero: "url('/what_we_do_hero.png')",
+				vision_hero: "url('/vision_hero.png')",
+				crafting_contemorary_hero: "url('/crafting_contemorary_hero.png')",
+				why_choose_hero: "url('/why_choose_hero.png')",
+				squares: "url('/squares.png')",
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
