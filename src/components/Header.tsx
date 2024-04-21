4e4010552dc19916358816effc3651f10ece5725
christoph-codes <tkcwebdev@gmail.com>
2024-04-21 14:26:32 -0700

    main: Add background color to header when scrolling down the page and remove once you reach the top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import Container from "./Container";
 import Navigation from "./Navigation";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const Header = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const position = window.scrollY;
+			if (position > 200) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		// Add scroll event listener
+		window.addEventListener("scroll", handleScroll);
+
+		// Clean up the event listener
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 	return (
-		<header className="fixed top-0 z-10 w-full">
+		<header
+			className={twMerge(
+				"fixed top-0 z-50 w-full transition-colors hover:bg-primary/90",
+				isScrolled && "bg-primary/40"
+			)}
+		>
 			<Container className="flex justify-between p-4 items-center">
 				<Link href="/">
 					<Image
